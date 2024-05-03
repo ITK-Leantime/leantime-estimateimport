e2d2804624cc0e33eb3b1fddad529c8cf146c33f
Jeppe Krogh <kjej@aarhus.dk>
2024-05-03 11:29:21 +0200

    Added conditions for displaying warnings and errors

--- Templates/importValidation.blade.php
+++ Templates/importValidation.blade.php
@@ -11,8 +11,8 @@
 <div class="maincontent">
     <?php echo $tpl->displayNotification(); ?>
     <?php
-    $mappings = $tpl->get('mappings');
-    $supportedFields = $tpl->get('supportedFields');
+$mappings = $tpl->get('mappings');
+$supportedFields = $tpl->get('supportedFields');
     ?>
     <div class="maincontentinner">
 
@@ -33,29 +33,42 @@
         <form enctype="multipart/form-data" action="" method="post">
 
         <?php
-        echo "<h2>Warnings:</h2>";
-        foreach($validationWarnings as $validationWarning) {
-            echo "<p>".$validationWarning."</p>";
-        }
-        echo "</br></br>";
-        echo "<h2>Errors:</h2>";
-        foreach($validationErrors as $errorGroupName => $errorGroup) {
-            echo "<h3>".$errorGroupName." (".count($errorGroup)." errors)</h3>";
-            foreach($errorGroup as $errorSubject => $errorSpec) {
-                echo "<p>".$errorSpec." <b>".$errorSubject."</b></p>";
+if (!empty($validationWarnings)) {
+  echo "<h2>Warnings:</h2>";
+  foreach ($validationWarnings as $validationWarning) {
+    echo "<p>" . $validationWarning . "</p>";
+  }
+}
+if (!empty($validationErrors)) {
+  echo "</br></br>";
+  echo "<h2>Errors:</h2>";
+  foreach ($validationErrors as $errorGroupName => $errorGroup) {
+    switch ($errorGroupName) {
+      case "Milestone":
+        echo "<div class='validation-error-headline'><h3>" . $errorGroupName . " (" . count($errorGroup) . " errors)</h3><span data-subject='" . $errorGroupName . "' class='btn btn-primary validation-fix-button'>Add milestones</span></div>";
+        break;
+      default:
+        echo "<div class='validation-error-headline'><h3>" . $errorGroupName . " (" . count($errorGroup) . " errors)</h3></div>";
+        break;
+    }
 
-            }
-        }
-        echo "</br></br>";
-        foreach($tpl->get('dataToValidate') as $count => $dataToValidate) {
-            echo "<h3>Ticket ".$count."</h3>";
-            foreach($dataToValidate as $field => $datumToValidate) {
-                $field = str_replace(' ', '_', $field);
-                echo "<p data-hest='".$field."'>".$supportedFields[$mappings[$field]]["name"]." -> ".$datumToValidate."</p>";
+    foreach ($errorGroup as $errorSubject => $errorSpec) {
+      echo "<p>" . $errorSpec . " <b>" . $errorSubject . "</b></p>";
 
-            }
-            echo "<br />";
-        }
+    }
+  }
+}
+
+echo "</br></br>";
+foreach ($tpl->get('dataToValidate') as $count => $dataToValidate) {
+  echo "<h3>Ticket " . $count . "</h3>";
+  foreach ($dataToValidate as $field => $datumToValidate) {
+    $field = str_replace(' ', '_', $field);
+    echo "<p data-hest='" . $field . "'>" . $supportedFields[$mappings[$field]]["name"] . " -> " . $datumToValidate . "</p>";
+
+  }
+  echo "<br />";
+}
         ?>
 
             <div class="form-group">
