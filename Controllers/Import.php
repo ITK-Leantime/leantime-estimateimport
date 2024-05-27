689bffc0de0577b7a222415ed6a68f1e61d4df78
Jeppe Krogh <kjej@aarhus.dk>
2024-05-27 07:58:34 +0200

    using tmp files instead of session for storing imported data

--- Controllers/Import.php
+++ Controllers/Import.php
@@ -2,6 +2,7 @@
 
 namespace Leantime\Plugins\EstimateImport\Controllers;
 
+use Exception;
 use Leantime\Core\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Leantime\Plugins\EstimateImport\Services\ImportHelper as ImportHelper;
@@ -13,26 +14,28 @@ class Import extends Controller
 {
     private ImportHelper $importHelper;
 
-  /**
-   * constructor
-   *
-   * @param ImportHelper $importHelper
-   * @return void
-   */
-    public function init(ImportHelper $importHelper)
+    /**
+     * constructor
+     *
+     * @param ImportHelper $importHelper
+     *
+     * @return void
+     */
+    public function init(ImportHelper $importHelper): void
     {
         $this->importHelper = $importHelper;
     }
-  /**
-   * get
-   *
-   * @return Response
-   *
-   * @throws \Exception
-   */
+
+    /**
+     * Gathers data and feeds it to the template.
+     *
+     * @return Response
+     *
+     * @throws Exception
+     */
     public function get(): Response
     {
-        unset($_SESSION['csv_data']);
+        unset($_SESSION['csv_data']['temp_fileName']);
 
         $importStyling = dirname($_SERVER['DOCUMENT_ROOT'], 2) . 'dist/css/plugin-EstimateImport.css';
         $importScript = dirname($_SERVER['DOCUMENT_ROOT'], 2) . 'dist/js/plugin-EstimateImport.js';
@@ -40,56 +43,82 @@ class Import extends Controller
         $this->tpl->assign('importScript', $importScript);
 
         $projectData = $this->importHelper->getAllProjectIds();
-        $this->tpl->assign('currentProject', $_SESSION['currentProject']);
+
+        // Get current project set in Leantime session
+        $currentProject = $_SESSION['currentProject'];
+
+        if (isset($currentProject)) {
+            $this->tpl->assign('currentProject', $_SESSION['currentProject']);
+        }
+
         $this->tpl->assign('projectData', $projectData);
 
         return $this->tpl->display('EstimateImport.import');
     }
 
-  /**
-   * post
-   *
-   * @param array<string, string|int> $params
-   * @return void
-   */
+    /**
+     * Handles the data submitted.
+     * Reads submitted file, settings and stores data in tmp file.
+     *
+     * @param array<string, string|int> $params
+     *
+     * @return void
+     *
+     * @throws Exception
+     */
     public function post(array $params): void
     {
-      // Read uploaded csv file
-        $fileEncoding = $params['fileEncoding'];
-        $estimateFile = fopen($_FILES['estimateFile']['tmp_name'], 'r, ' . $fileEncoding);
+        $estimateFileType = pathinfo($_FILES['estimateFile']['name'], PATHINFO_EXTENSION);
+        $estimateFile = $_FILES['estimateFile']['tmp_name'];
 
-        if (!$estimateFile) {
-            die('Unable to open file.');
+        if ($estimateFileType !== 'csv' || !$estimateFile) {
+            error_log("No file or wrong extension");
+            throw new Exception('No file or wrong extension');
+        }
+        $estimateFileReader = fopen($estimateFile, 'r');
+
+        if (!$estimateFileReader) {
+            error_log("Cannot open file");
+            throw new Exception('Cannot open file');
         }
 
-      // Loop rows and grab content
+        // Loop rows and grab content
         $estimateData = [];
-        $delimiter = $params['delimiter'];
+        $delimiter = $params['delimiter'] ?? ';';
 
-        while (($row = fgetcsv($estimateFile, 0, $delimiter)) !== false) {
+        while (($row = fgetcsv($estimateFileReader, 0, $delimiter)) !== false) {
             $estimateData[] = $row;
         }
-        fclose($estimateFile);
+        fclose($estimateFileReader);
 
+        if (empty($estimateData)) {
+            error_log("Cannot open file");
+            throw new Exception('Cannot open file');
+        }
         $estimateHeaders = array_shift($estimateData);
 
-      // Replace numeric array keys with headers from data
+        // Replace numeric array keys with headers from data
         $estimateDataResult = array_map(function ($item) use ($estimateHeaders) {
-          // Remove empty headers
+            // Remove empty headers
             $filteredHeaders = array_filter($estimateHeaders, function ($estimateHeader) {
                 return !empty(trim($estimateHeader));
             });
-            $itemWithHeaders = array_combine($filteredHeaders, array_intersect_key($item, $filteredHeaders));
-            return $itemWithHeaders;
+            return array_combine($filteredHeaders, array_intersect_key($item, $filteredHeaders));
         }, $estimateData);
 
-      // Save data to session
-        $_SESSION['csv_data']['data'] = $estimateDataResult;
-        $_SESSION['csv_data']['headers'] = $estimateHeaders;
-        $_SESSION['csv_data']['project_id'] = $params['projectId'];
-        $_SESSION['csv_data']['date_format'] = $params['dateFormat'];
+        $dataToStore = [
+            'data' => $estimateDataResult,
+            'headers' => $estimateHeaders,
+            'projectId' => $params['projectId'],
+            'dateFormat' => $params['dateFormat']
+        ];
 
-      // Redirect to next step
+        $tmpFile = $this->importHelper->saveDataToTempFile(false, $dataToStore);
+
+        // Save tmp file location to session
+        $_SESSION['csv_data']['temp_fileName'] = $tmpFile;
+
+        // Redirect to next step
         header('Location: /EstimateImport/importMapping');
     }
 }
