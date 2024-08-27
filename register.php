3fe3615c4754fa71fb5f07c69978938cc673f5de
Jeppe Krogh <kjej@aarhus.dk>
2024-08-27 12:25:17 +0200

    Using new EventDispatcher for register.php

--- register.php
+++ register.php
@@ -1,6 +1,6 @@
 <?php
 
-use Leantime\Core\Events;
+use Leantime\Core\Events\EventDispatcher;
 
 /**
 * Adds a menu point for adding fixture data.
@@ -22,4 +22,4 @@ function addImportDataMenuPoint(array $menuStructure): array
     return $menuStructure;
 }
 
-Events::add_filter_listener('leantime.domain.menu.repositories.menu.getMenuStructure.menuStructures', 'addImportDataMenuPoint');
+EventDispatcher::add_filter_listener('leantime.domain.menu.repositories.menu.getMenuStructure.menuStructures', 'addImportDataMenuPoint');
